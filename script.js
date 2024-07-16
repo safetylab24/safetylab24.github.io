bc5a724f6afeadeb4efa22352d0e6fb590701528
safetylab24 <safetylab24@gmail.com>
2024-07-16 14:32:29 -0500

    Updated script.js

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-document.getElementById('clickMe').addEventListener('click', () => {
-    document.getElementById('response').textContent = 'Button was clicked!';
+document.addEventListener('DOMContentLoaded', function() {
+    const customCursorBorder = document.querySelector('.custom-cursor-border');
+    customCursorBorder.style.width = '22px';
+    customCursorBorder.style.height = '22px';
+    const customCursor = document.querySelector('.custom-cursor');
+    customCursor.style.width = '20px';
+    customCursor.style.height = '20px';
+
+    
+    document.addEventListener('mousemove', function(e) {
+        const cursorX = e.clientX;
+        const cursorY = e.clientY;
+
+        customCursor.style.left = cursorX + 'px';
+        customCursor.style.top = cursorY + 'px';
+
+        customCursorBorder.style.left = cursorX + 'px';
+        customCursorBorder.style.top = cursorY + 'px';
+    });
+
+    document.addEventListener('mousedown', function() {
+        customCursor.style.width = '58px';
+        customCursor.style.height = '58px';
+
+        customCursorBorder.style.width = '62px';
+        customCursorBorder.style.height = '62px';
+    });
+
+    
+    document.addEventListener('mouseup', function() {
+        customCursor.style.width = '20px';
+        customCursor.style.height = '20px';
+
+        customCursorBorder.style.width = '22px';
+        customCursorBorder.style.height = '22px';
+    });
+    
 });
