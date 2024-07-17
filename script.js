17f46b6b23ab680824640da4ea5f23875210cf77
Pranav <pxg082007@gmail.com>
2024-07-17 13:29:18 -0500

    updated all -p

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const customCursorBorder = document.querySelector('.custom-cursor-border');
-    customCursorBorder.style.width = '22px';
-    customCursorBorder.style.height = '22px';
+    customCursorBorder.style.width = '16px';
+    customCursorBorder.style.height = '16px';
 
     const customCursor = document.querySelector('.custom-cursor');
-    customCursor.style.width = '20px';
-    customCursor.style.height = '20px';
+    customCursor.style.width = '14px';
+    customCursor.style.height = '14px';
 
     
     document.addEventListener('mousemove', function(e) {
@@ -25,18 +25,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
         customCursorBorder.style.width = '42px';
         customCursorBorder.style.height = '42px';
-
-        customCursor.style.opacity = '0.3';
-        customCursorBorder.style.opacity = '0.3';
+        
+        customCursor.style.opacity = '0.25';
+        customCursorBorder.style.opacity = '0.25';
     });
 
     
     document.addEventListener('mouseup', function() {
-        customCursor.style.width = '20px';
-        customCursor.style.height = '20px';
+        customCursor.style.width = '14px';
+        customCursor.style.height = '14px';
 
-        customCursorBorder.style.width = '22px';
-        customCursorBorder.style.height = '22px';
+        customCursorBorder.style.width = '16px';
+        customCursorBorder.style.height = '16px';
 
         customCursor.style.opacity = '1';
         customCursorBorder.style.opacity = '1';
