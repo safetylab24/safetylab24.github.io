ae0d113573252f5e5a05980677a2dbba805ea7ca
Pranav <pxg082007@gmail.com>
2024-07-22 22:24:32 -0500

    finished website

--- script.js
+++ script.js
@@ -8,66 +8,97 @@ document.addEventListener('DOMContentLoaded', function() {
     customCursor.style.height = '6px';
 
     document.addEventListener('mousemove', function(e) {
-      const cursorX = e.clientX;
-      const cursorY = e.clientY;
+        const cursorX = e.clientX;
+        const cursorY = e.clientY;
 
-      customCursor.style.left = cursorX + 'px';
-      customCursor.style.top = cursorY + 'px';
+        customCursor.style.left = cursorX + 'px';
+        customCursor.style.top = cursorY + 'px';
 
-      customCursorBorder.style.left = cursorX + 'px';
-      customCursorBorder.style.top = cursorY + 'px';
+        customCursorBorder.style.left = cursorX + 'px';
+        customCursorBorder.style.top = cursorY + 'px';
     });
 
     document.addEventListener('mousedown', function() {
-      customCursor.style.width = '40px';
-      customCursor.style.height = '40px';
+        customCursor.style.width = '40px';
+        customCursor.style.height = '40px';
 
-      customCursorBorder.style.width = '42px';
-      customCursorBorder.style.height = '42px';
-      
-      customCursor.style.opacity = '0.25';
-      customCursorBorder.style.opacity = '0.25';
+        customCursorBorder.style.width = '42px';
+        customCursorBorder.style.height = '42px';
+        
+        customCursor.style.opacity = '0.25';
+        customCursorBorder.style.opacity = '0.25';
     });
 
     document.addEventListener('mouseup', function() {
-      customCursor.style.width = '6px';
-      customCursor.style.height = '6px';
+        customCursor.style.width = '6px';
+        customCursor.style.height = '6px';
 
-      customCursorBorder.style.width = '10px';
-      customCursorBorder.style.height = '10px';
+        customCursorBorder.style.width = '10px';
+        customCursorBorder.style.height = '10px';
 
-      customCursor.style.opacity = '1';
-      customCursorBorder.style.opacity = '1';
+        customCursor.style.opacity = '1';
+        customCursorBorder.style.opacity = '1';
     });
 
     const elements = document.querySelectorAll('.keypoint');
-      elements.forEach(element => {
+    elements.forEach(element => {
         element.addEventListener("mouseover", (event) => {
-          event.target.style.width = '1.5vw';
-          event.target.style.height = '1.5vw';
+            event.target.style.width = '1.5vw';
+            event.target.style.height = '1.5vw';
 
-          event.target.style.translate = '-0.5vw -0.5vw';
+            event.target.style.translate = '-0.25vw -0.25vw';
         });
 
         element.addEventListener("mouseout", (event) => {
-          event.target.style.width = '0.5vw';
-          event.target.style.height = '0.5vw';
+            event.target.style.width = '1vw';
+            event.target.style.height = '1vw';
 
-          event.target.style.translate = '0vw 0vw';
+            event.target.style.translate = '0vw 0vw';
         });
     });
-});
 
-const span = document.querySelector("span");
+    var keypoints = ["keypoint1", "keypoint2", "keypoint3", "keypoint4"];
+    keypoints.forEach(keypoint => {
+        const key = document.getElementById(String(keypoint));
+        
+        key.addEventListener("mouseover", (event) => {
+            document.getElementById("back-left").src="images/" + String(keypoint) + "/back-left.png";
+            document.getElementById("back-right").src="images/" + String(keypoint) + "/back-right.png";
+            document.getElementById("front-left").src="images/" + String(keypoint) + "/front-left.png";
+            document.getElementById("front-right").src="images/" + String(keypoint) + "/front-right.png";
+            document.getElementById("front").src="images/" + String(keypoint) + "/front.png";
+            document.getElementById("back").src="images/" + String(keypoint) + "/back.png";
+        });
 
-span.onclick = function() {
-  document.execCommand("copy");
-}
+        key.addEventListener("mouseout", (event) => {
+            document.getElementById("back-left").src="images/emptyView/back-left.png";
+            document.getElementById("back-right").src="images/emptyView/back-right.png";
+            document.getElementById("front-left").src="images/emptyView/front-left.png";
+            document.getElementById("front-right").src="images/emptyView/front-right.png";
+            document.getElementById("front").src="images/emptyView/front.png";
+            document.getElementById("back").src="images/emptyView/back.png";
+        });
+    });
 
-span.addEventListener("copy", function(event) {
-  event.preventDefault();
-  if (event.clipboardData) {
-    event.clipboardData.setData("text/plain", span.textContent);
-    console.log(event.clipboardData.getData("text"))
-  }
+    const copyToClipboard = str => {
+                if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+                    return navigator.clipboard.writeText(str);
+                }
+                return Promise.reject('The Clipboard API is not available.');
+            };
+
+            const copyTextElement = document.getElementById('copy-text');
+
+            if (copyTextElement) {
+                copyTextElement.addEventListener('click', () => {
+                    const email = "safetylab24@gmail.com";
+                    copyToClipboard(email).then(() => {
+                        alert('Text copied to clipboard: ' + email);
+                    }).catch(err => {
+                        console.error('Failed to copy text to clipboard:', err);
+                    });
+                });
+            } else {
+                console.error('Element with id "copy-text" not found.');
+            }
 });
